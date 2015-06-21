fef1a543e4d79a2e350ddc12dc295e70d5015aa4
samuelfabel <samuelfabel@hotmail.com>
2015-06-21 20:11:42 -0300

    Inclusão do exercicio de função de solve cacheada
    Inclusão do exercicio de função de solve cacheada

---
+++ cachematrix.R
@@ -0,0 +1,31 @@
+## Return a cached matrix function
+
+makeCacheMatrix <- function(x = matrix()) {
+  s <- NULL
+  set <- function(y) {
+    x <<- y
+    s <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) s <<- solve
+  getsolve <- function() s
+  
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
+}
+
+
+## Return a inversed matrix
+
+cacheSolve <- function(x) {
+  s <- x$getsolve()
+  if(!is.null(s)) {
+    message("getting cached data")
+    return(s)
+  }
+  data <- x$get()
+  s <- solve(data)
+  x$setsolve(s)
+  s
+}
